2aeb57e6fca532597d9b2c5065244a95a6487bac
shariq583 <gazdarshariq0@gmail.com>
2024-03-24 21:03:02 +0500

    ...

--- Main.js
+++ Main.js
@@ -1,9 +1,5 @@
 let logo = document.querySelector("#logoImg");
-let searchIco = document.querySelector("#searchIcon");
+
 logo.addEventListener("click", () => {
   window.location.href = "landingPage.html";
-});
-searchIco.addEventListener("click", () => {
-  console.log("searchVal");
-  window.location.href = "resultPage.html";
 });
